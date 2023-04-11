beb396f4ebeaaa7609d26daf434290fbb248cb9c
Diego <email.diegovieira@gmail.com>
2023-04-11 00:21:03 -0300

    last

--- api/src/controllers/auth.js
+++ api/src/controllers/auth.js
@@ -6,48 +6,10 @@ const router = express.Router();
 
 router.post('/login', async (req, res, next) => {
   try {
-
-    let { mail, password, google } = req.body;
-
-    let user;
-
-    if (!google) {
-      user = await login(req.pgConn, mail, password);
-    } else {
-      user = await loginGoogle(req.pgConn, google.profileObj.email);
-    }
-
-
     res.status(200).json({ ...user, auth: await createAuth(user) });
-
   } catch (error) {
     next(error)
   }
 });
-
-router.post('/requestreset', async (req, res, next) => {
-  try {
-
-    let { mail } = req.body;
-
-    await createReset(req.pgConn, mail)
-
-    res.status(200).json();
-
-  } catch (error) {
-    next(error)
-  }
-}); 
-
-router.post('/confirmreset', async (req, res, next) => {
-  try {
-    const {token, password} = req.body;
-    await confirmReset(req.pgConn, token, password);
-
-    res.status(200).json();
-  } catch (error) {
-    next(error)
-  }
-})
 
 module.exports = router
--- api/src/routes.js
+++ api/src/routes.js
@@ -1,25 +1,9 @@
-const auth = require('./controllers/auth');
-const user = require('./controllers/user');
-const unity = require('./controllers/unity');
-const people = require('./controllers/people');
-const access = require('./controllers/access');
-const config = require('./controllers/config');
-const logs = require('./controllers/logs');
-const cep = require('./controllers/cep');
-const dashboard = require('./controllers/dashboard');
-const firmware = require('./controllers/firmware');
+
 const { privateRoute, adminRoute } = require('./lib/auth');
 
 
 module.exports = (app) => {
   app.use('/auth', auth);
-  app.use('/user', privateRoute, adminRoute, user);
-  app.use('/unity', privateRoute, unity);
-  app.use('/logs', privateRoute, logs);
-  app.use('/people', privateRoute, people);
-  app.use('/config', privateRoute, config);
-  app.use('/access', privateRoute, access);
-  app.use('/cep', privateRoute, cep);
-  app.use('/dashboard', privateRoute, dashboard);
-  app.use('/firmware', privateRoute, firmware);
+  // app.use('/user', privateRoute, adminRoute, user);
+
 }
